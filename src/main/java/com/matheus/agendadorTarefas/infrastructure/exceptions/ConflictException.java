7be8e58ac52ecec204d91e4d2d2586fcddb5b247
Matheus Rodrigues <mathteusrrt1@gmail.com>
2026-02-24 11:35:01 -0300

    adicionadasd configurações de segurança

---
+++ src/main/java/com/matheus/agendadorTarefas/infrastructure/exceptions/ConflictException.java
@@ -0,0 +1,12 @@
+package com.matheus.agendadorTarefas.infrastructure.exceptions;
+
+public class ConflictException extends RuntimeException{
+
+    public ConflictException(String msg) {
+        super(msg);
+    }
+
+    public ConflictException(String msg, Throwable throwable) {
+        super(msg, throwable);
+    }
+}
